00e14bd69358012f1f1bb64634fc746796b856df
Chris Chalstrom <cchalstr@nsidc.org>
2014-08-26 09:56:19 -0600

    Add working grunt task to create dist/ with grunt dist.  The default grunt task fails because unit tests do not pass, and grunt dist fails because it is expecting grunt concat to be run first.

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,6 +118,7 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('dist', ['concat', 'uglify']);
   grunt.registerTask('docs', ['default', 'exec']);
 };
 
